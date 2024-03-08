85e64ebc55356f86f4279d615f7d3d83254c7e37
Yinan Qin <39023210+ganyuanzhen@users.noreply.github.com>
2024-03-08 13:14:38 +0800

    实现自动安装依赖，无需确认

--- build-github.sh
+++ build-github.sh
@@ -26,7 +26,7 @@ mkdir -p $deb_dir
 
 function InstallDepends() {
     echo '开始安装依赖'
-    apt install git  devscripts equivs
+    apt-get --yes install git  devscripts equivs
     git clone https://github.com/LingmoOS/LingmoOSBuildDeps.git
     cd LingmoOSBuildDeps
     mk-build-deps -i -t "apt-get -y" -r  > /dev/null
